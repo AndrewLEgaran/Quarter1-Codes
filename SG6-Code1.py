2acae74fa89d854ae4b0aff7609c4748cb7ec2bc
Andrew Lester L. Egaran <andrewlester.egaran@pshs.smc.edu.ph>
2025-09-09 10:25:28 +0800

    SG6 Adding Decimals and Rounding Off to 2 Decimals

---
+++ SG6-Code1.py
@@ -0,0 +1,5 @@
+Guest1 = float(input("PLease enter a number, user1:"))
+Guest2 = float(input("PLease enter a number, user1:"))
+Guest3 = float(input("PLease enter a number, user1:"))
+sum = Guest1 + Guest2 + Guest3
+print (round(sum,2))
