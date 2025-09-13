5cbb9475e5f41e13adbc54bb48b506e00bd68341
Andrew Lester L. Egaran <andrewlester.egaran@pshs.smc.edu.ph>
2025-09-13 21:07:47 +0800

    Reformatting names

--- SG6-Code2.py
+++ SG6-Code2.py
@@ -1,3 +1,5 @@
+import math
+
 Radius = float(input("Please enter the radius of the circle: "))
-Area = 3.1415 * Radius * Radius
+Area = math.pi * Radius * Radius
 print("The area of the circle is:", round(Area, 2))
